4817044ee565902801806fd2b96cefc6c90a2308
macle86 <ysys86a@gmail.com>
2022-02-26 11:13:44 +0900

    2022

--- modules/technical-analysis/src/main/java/io/runon/trading/technical/analysis/indicator/Disparity.java
+++ modules/technical-analysis/src/main/java/io/runon/trading/technical/analysis/indicator/Disparity.java
@@ -1,7 +1,43 @@
 package io.runon.trading.technical.analysis.indicator;
+
+import io.runon.trading.Price;
+import io.runon.trading.technical.analysis.indicator.ma.MovingAverage;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * 이격도
  * @author macle
  */
 public class Disparity {
+    /**
+     * 이격도 얻기
+     */
+    public BigDecimal get(Price[] prices, int avgCount){
+
+        return get(prices[prices.length-1], prices, avgCount);
+    }
+
+    /**
+     * 이격도 얻기
+     */
+    public BigDecimal get(Price price, Price[] prices, int avgCount){
+        return price.getClose().divide(MovingAverage.getAverage(prices, avgCount) , 4, RoundingMode.HALF_UP).stripTrailingZeros();
+    }
+
+
+    /**
+     * 이격도 얻기
+     */
+    public BigDecimal get(BigDecimal[] prices, int avgCount){
+        return get(prices[prices.length-1], prices, avgCount);
+    }
+
+    /**
+     * 이격도 얻기
+     */
+    public BigDecimal get(BigDecimal price, BigDecimal[] prices, int avgCount){
+        return price.divide(MovingAverage.getAverage(prices, avgCount) , 4, RoundingMode.HALF_UP).stripTrailingZeros();
+    }
 }
