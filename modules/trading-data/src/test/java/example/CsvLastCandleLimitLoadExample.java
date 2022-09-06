7f2b7f63188fc7a80e3371851c93bea0decf0959
macle86 <ysys86a@gmail.com>
2022-09-06 21:44:01 +0900

    2022

---
+++ modules/trading-data/src/test/java/example/CsvLastCandleLimitLoadExample.java
@@ -0,0 +1,19 @@
+package example;
+
+import com.seomse.commons.utils.time.Times;
+import io.runon.trading.CandleTimes;
+import io.runon.trading.data.csv.CsvCandle;
+import io.runon.trading.technical.analysis.candle.TradeCandle;
+
+/**
+ * @author macle
+ */
+public class CsvLastCandleLimitLoadExample {
+    public static void main(String[] args) {
+        String path =  "D:\\data\\cryptocurrency\\futures\\candle\\BTCUSDT\\5m";
+        TradeCandle [] candles = CsvCandle.load(path, Times.MINUTE_5, 500);
+        for (TradeCandle tradeCandle : candles){
+            System.out.println(Times.ymdhm(tradeCandle.getOpenTime(), CandleTimes.KOREAN_STOCK_ZONE_ID));
+        }
+    }
+}
