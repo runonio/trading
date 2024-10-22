edfac0d97d9bed0b008f445040a43aa998e5c98e
macle <ysys86a@gmail.com>
2024-10-22 18:55:31 +0900

    2024

--- modules/trading/src/main/java/io/runon/trading/data/StringArray.java
+++ modules/trading/src/main/java/io/runon/trading/data/StringArray.java
@@ -13,6 +13,8 @@ public class StringArray {
     public StringArray(String ...array){
         this.array = array;
     }
+
+
     public String[] getArray() {
         return array;
     }
@@ -29,4 +31,21 @@ public class StringArray {
     public String toString(){
         return GsonUtils.GSON.toJson(array);
     }
+
+    public boolean containsArrays(String text){
+
+        if(array == null || array.length==0){
+            return false;
+        }
+
+        for(String a : array){
+            if(!text.contains(a)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
 }
